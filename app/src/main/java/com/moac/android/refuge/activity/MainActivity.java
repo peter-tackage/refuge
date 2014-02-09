3a94a7f22dc050d8ea2515aacce6f4dd4416fe56
peter.tackage <peter.tackage@gmail.com>
2014-02-09 11:24:08 +0100

    Add search mechanism, without the results

--- app/src/main/java/com/moac/android/refuge/activity/MainActivity.java
+++ app/src/main/java/com/moac/android/refuge/activity/MainActivity.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 ;
 import android.app.ActionBar;
 import android.app.FragmentManager;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.widget.DrawerLayout;
+import android.view.View;
 import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -21,13 +26,10 @@ import com.moac.android.refuge.RefugeApplication;
 import com.moac.android.refuge.database.ModelService;
 import com.moac.android.refuge.fragment.NavigationDrawerFragment;
 import com.moac.android.refuge.R;
-import com.moac.android.refuge.importer.DOMFileImporter;
-import com.moac.android.refuge.importer.LoadDataRunnable;
 import com.moac.android.refuge.model.Country;
 import com.moac.android.refuge.model.RefugeeFlow;
-import com.moac.android.refuge.util.DoOnce;
 
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -46,10 +48,8 @@ public class MainActivity extends Activity
     @Inject
     ModelService mModelService;
     private GoogleMap mMap;
-    private MapFragment mMapFragment;
-    private Handler mHandler;
     private SearchView mSearchView;
-    private static final long MAX_RADIUS = 2000000; // 2000 kms
+    private static final long MAX_RADIUS = 1500000; // 1500 kms
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,115 +70,96 @@ public class MainActivity extends Activity
         mMap = ((MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map)).getMap();
 
+        // We are using single top mode, so this will not contain
+        // search intents as the SearchView is operating on its host
+        // Activity - instead see onNewIntent()
+        handleIntent(getIntent());
 
-        String assetFile = "UNDataExport2012.xml";
-        try {
-            boolean attemptedToLoad = DoOnce.doOnce(this, LOAD_DATA_TASK_TAG, new LoadDataRunnable(new DOMFileImporter(mModelService), getAssets().open(assetFile)));
-            Log.i(TAG, "Attempted to load data: " + attemptedToLoad);
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to open the data file: " + assetFile, e);
-            finish();
-        }
+//        String assetFile = "UNDataExport2012.xml";
+//        try {
+//            boolean attemptedToLoad = DoOnce.doOnce(this, LOAD_DATA_TASK_TAG, new LoadDataRunnable(new DOMFileImporter(mModelService), getAssets().open(assetFile)));
+//            Log.i(TAG, "Attempted to load data: " + attemptedToLoad);
+//        } catch (IOException e) {
+//            Log.e(TAG, "Failed to open the data file: " + assetFile, e);
+//            finish();
+//        }
 
-        List<Country> allCountries = mModelService.getAllCountries();
-        Log.i(TAG, "We loaded these countries: " + allCountries);
-        for(Country c : allCountries) {
-            Log.i(TAG, "Country: " + c.getName());
-            List<RefugeeFlow> flows = mModelService.getRefugeeFlowsFrom(c.getId());
-            Log.i(TAG, "Country has flows into: " + c.getName());
+//        List<Country> allCountries = mModelService.getAllCountries();
+//        Log.i(TAG, "We loaded these countries: " + allCountries);
+//        for(Country c : allCountries) {
+//            Log.i(TAG, "Country: " + c.getName());
+//            List<RefugeeFlow> flows = mModelService.getRefugeeFlowsFrom(c.getId());
+//            Log.i(TAG, "Country has flows into: " + c.getName());
+//
+//        }
 
-        }
-      //  drawCountries(allCountries);
+        List<Country> selectedCountries = new ArrayList<Country>();
+        selectedCountries.add(mModelService.getCountry(0));
+        selectedCountries.add(mModelService.getCountry(6));
+        drawCountries(selectedCountries);
     }
 
-//    private void insertTestData() {
-//        List<Country> countryList = new ArrayList<Country>();
-//
-//        Country au = new Country("Australia");
-//        au.setLatLng(-45, 90);
-//        countryList.add(au);
-//        long auId = mModelService.createCountry(au);
-//
-//        Country af = new Country("Afghanistan");
-//        af.setLatLng(45, 90);
-//        countryList.add(af);
-//        long afId = mModelService.createCountry(af);
-//
-//
-//        Country iq = new Country("Iraq");
-//        iq.setLatLng(-45, -90);
-//        countryList.add(iq);
-//        long iqId = mModelService.createCountry(iq);
-//
-//        RefugeeFlow af2au = new RefugeeFlow(af, au);
-//        af2au.setRefugeeCount(1000);
-//        af2au.setYear(2012);
-//        mModelService.createCountry(af2au);
-//
-//        RefugeeFlow iq2au = new RefugeeFlow(iq, au);
-//        iq2au.setRefugeeCount(750);
-//        iq2au.setYear(2012);
-//        mModelService.createCountry(iq2au);
-//    }
 
     private void drawCountries(List<Country> countries) {
+        Log.i(TAG, "drawCountries() - Draw TO countries: " + countries);
         long maxRefugeeFlowTo = 0;
         for (Country country : countries) {
             maxRefugeeFlowTo = Math.max(maxRefugeeFlowTo,
                     mModelService.getTotalRefugeeFlowTo(country.getId()));
         }
 
+        final int[] colors =
+                {0xAA434B52, 0xAA54B395, 0xAAD6B331,
+                        0xAAA465C5, 0xAA5661DE, 0xAA4AB498, 0xAAFA7B68, 0xAAFF6600,
+                        0xAA669900, 0xAA66CCCC};
+
         // Maximum radius is defined
-        for (Country country : countries) {
-            drawFromCircles(country.getId(), 0xAA4AB498, maxRefugeeFlowTo);
+        for (Country toCountry : countries) {
+            drawAllFromCircles(toCountry.getId(), colors[(int) toCountry.getId()], maxRefugeeFlowTo);
+            drawToCircle(toCountry.getId(), colors[(int) toCountry.getId()], mModelService.getTotalRefugeeFlowTo(toCountry.getId()), maxRefugeeFlowTo);
+
         }
+        Log.i(TAG, "drawCountries() - Calculated maxRefugeeFlowTo: " + maxRefugeeFlowTo);
     }
 
-    private void drawFromCircles(long toCountryId, int toCountryColor, long maxFlow) {
-        List<RefugeeFlow> flows = mModelService.getRefugeeFlowsFrom(toCountryId);
+    private void drawAllFromCircles(long toCountryId, int toCountryColor, long maxCount) {
+        Log.i(TAG, "drawAllFromCircles() - toCountryId: " + toCountryId + " toCountryColor: " + toCountryColor + " maxFlow: " + maxCount);
+        List<RefugeeFlow> flows = mModelService.getRefugeeFlowsTo(toCountryId);
         for (RefugeeFlow flow : flows) {
             Country fromCountry = mModelService.getCountry(flow.getFromCountry().getId());
-            LatLng coordinates = new LatLng(fromCountry.getLatitude(), fromCountry.getLongitude());
-            renderOutgoingFlowCircle(mMap, coordinates, maxFlow, flow.getRefugeeCount(), toCountryColor);
+            Log.i(TAG, "drawAllFromCircles() - Drawing flow from: " + fromCountry.getName());
+            drawScaledCircle(mMap, fromCountry.getLatLng(), flow.getRefugeeCount(), maxCount, toShade(toCountryColor), toCountryColor);
         }
     }
 
-    public static Circle renderOutgoingFlowCircle(GoogleMap map,
-                                                  LatLng coordinates,
-                                                  long maxSelectedCount,
-                                                  long count,
-                                                  int color) {
+    private void drawToCircle(long toCountryId, int toCountryColor, long countTo, long maxCountTo) {
+        Country toCountry = mModelService.getCountry(toCountryId);
+        drawScaledCircle(mMap, toCountry.getLatLng(), countTo, maxCountTo, toCountryColor, toShade(toCountryColor));
+    }
 
-        long radius = (count / maxSelectedCount) * MAX_RADIUS;
+    public static Circle drawScaledCircle(GoogleMap map,
+                                          LatLng coordinates,
+                                          long count, long maxCount,
+                                          int strokeColor, int fillColor) {
+        Log.i(TAG, "drawScaledCircle() - count: " + count);
+        long radius = (long) (((double) count / (double) maxCount) * MAX_RADIUS);
+        Log.i(TAG, "drawScaledCircle() - radius (m): " + radius);
         CircleOptions circleOptions = new CircleOptions()
                 .center(coordinates)
                 .radius(radius)
-                .fillColor(color)
-                .strokeWidth(0); // In meters
-
+                .fillColor(fillColor)
+                .strokeColor(strokeColor)
+                .strokeWidth(5);
         return map.addCircle(circleOptions);
     }
 
-//    private class AnimateRunnable implements Runnable {
-//
-//        private final Circle mCircle;
-//
-//        public AnimateRunnable(Circle c) {
-//            mCircle = c;
-//        }
-//        @Override
-//        public void run() {
-//
-//          //  mCircle.setRadius(mCircle.getRadius() - 10000);
-//            LatLng ll = mCircle.getCenter();
-//            mCircle.setCenter(new LatLng(ll.latitude, ll.longitude + 0.5));
-//            mHandler.postDelayed(this, 100);
-////            mCircle.setFillColor(mCircle.getFillColor() + 1);
-////                    mHandler.postDelayed(this, 1);
-//        }
-//    }
-
-
+    private static int toShade(int _color) {
+        float[] hsv = new float[3];
+        Color.colorToHSV(_color, hsv);
+        hsv[1] *= 0.5;
+        hsv[2] *= 1.5;
+        return Color.HSVToColor(hsv);
+    }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
@@ -205,26 +186,24 @@ public class MainActivity extends Activity
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
             getMenuInflater().inflate(R.menu.main, menu);
+
+            // Get the SearchView and set the searchable configuration
+            SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            mSearchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+            mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            mSearchView.setIconifiedByDefault(true);
+            mSearchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (v == mSearchView && !hasFocus) mSearchView.setIconified(true);
+                }
+            });
+            // Note: I don't register callbacks to invoke the search query - use the Intents instead.
+
             restoreActionBar();
             return true;
         }
-//        // Inflate the options menu from XML
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.options_menu, menu);
-//
-//        // Get the SearchView and set the searchable configuration
-//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        mSearchView = (SearchView) menu.findItem(R.id.menu_item_search).getActionView();
-//        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-//        mSearchView.setIconifiedByDefault(true);
-//        mSearchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-//            @Override public void onFocusChange(View v, boolean hasFocus) {
-//                if(v == mSearchView && !hasFocus) mSearchView.setIconified(true);
-//            }
-//        });
-//        // Note: I don't register callbacks to invoke the search query - use the Intents instead.
-//
-//        return true;
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -234,75 +213,30 @@ public class MainActivity extends Activity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
 
-    /**
-     *
-     private static final String TAG = MainActivity.class.getSimpleName();
-     RefugeMapFragment mMapFragment;
-     SearchView mSearchView;
+    @Override
+    protected void onNewIntent(Intent _intent) {
+        Log.i(TAG, "onNewIntent - received intent");
+        //  Removed this as causes the fragment to reperform search on rotation.
+        // setIntent(_intent);
+        handleIntent(_intent);
+    }
 
-     @Override protected void onCreate(Bundle savedInstanceState) {
-     super.onCreate(savedInstanceState);
-     Log.v(TAG, "onCreate()- start");
-     setContentView(R.layout.activity_main);
+    private void handleIntent(Intent _intent) {
+        if (Intent.ACTION_SEARCH.equals(_intent.getAction())) {
+            String query = _intent.getStringExtra(SearchManager.QUERY).trim();
+            // FIXME doSearch(query);
+            Toast.makeText(this, "Searching for: " + query, Toast.LENGTH_LONG).show();
+            mModelService.getCountryByName(query);
+        }
+    }
 
-     // Get reference to MapFragment
-     mMapFragment = (RefugeMapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
-
-     // We are using single top mode, so this will not contain
-     // search intents as the SearchView is operating on its host
-     // Activity - instead see onNewIntent()
-     handleIntent(getIntent());
-     }
-
-     @Override protected void onNewIntent(Intent _intent) {
-     Log.i(TAG, "onNewIntent - received intent");
-     //  Removed this as causes the fragment to reperform search on rotation.
-     // setIntent(_intent);
-     handleIntent(_intent);
-     }
-
-     private void handleIntent(Intent _intent) {
-     if(Intent.ACTION_SEARCH.equals(_intent.getAction())) {
-     String query = _intent.getStringExtra(SearchManager.QUERY);
-     doSearch(query);
-     }
-     }
-
-     @Override public void onSaveInstanceState(Bundle _outstate) {
-     super.onSaveInstanceState(_outstate);
-     }
-
-     @Override public boolean onCreateOptionsMenu(Menu menu) {
-     // Inflate the options menu from XML
-     MenuInflater inflater = getMenuInflater();
-     inflater.inflate(R.menu.options_menu, menu);
-
-     // Get the SearchView and set the searchable configuration
-     SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-     mSearchView = (SearchView) menu.findItem(R.id.menu_item_search).getActionView();
-     mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-     mSearchView.setIconifiedByDefault(true);
-     mSearchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-     @Override public void onFocusChange(View v, boolean hasFocus) {
-     if(v == mSearchView && !hasFocus) mSearchView.setIconified(true);
-     }
-     });
-     // Note: I don't register callbacks to invoke the search query - use the Intents instead.
-
-     return true;
-     }
-
-     private void doSearch(String _query) {
-     Log.i(TAG, "doSearch() - query: " + _query);
-     mMapFragment.doSearch(_query);
-     }
-     */
+    // TODO SaveInstanceState
 
 }
